f916984866d223bde8ccc117f0df2772b6a48c04
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-12-20 00:28:12 +0530

    data-process-stage-1

--- src/data/data-process.js
+++ src/data/data-process.js
@@ -2,22 +2,30 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const path = require("path");
 
-const fileDir = path.join(__dirname, "./covid-data");
+const fileDir = path.join(__dirname, "./covid-data-sample");
 
 const files = fs
   .readdirSync(fileDir)
   .sort((a, b) => new Date(new Date(b.slice(0, 10))) - new Date(a.slice(0, 10)))
   .reverse();
 const resultFile = path.join(__dirname, "./data.json");
-const results = [];
+const results = {};
 
 files.forEach((file) => {
   const filePath = path.join(__dirname, `./covid-data/${file}`);
   fs.createReadStream(filePath)
     .pipe(csv())
-    .on("data", (entry) => {
-      if (entry["Country_Region"] === "India") {
-        results.push(entry);
+    .on("data", (data) => {
+      if (data["Country_Region"] === "India") {
+        if (results[data["Province_State"]]) {
+          results[data["Province_State"]].history.push({
+            [file.slice(0, 10)]: data,
+          });
+        } else {
+          results[data["Province_State"]] = {
+            history: [{ [file.slice(0, 10)]: data }],
+          };
+        }
       }
     })
     .on("end", () => {
